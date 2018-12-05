e65f255a5bd96efc5d96871772c1c6ae913e1c38
himself65 <himself6565@gmail.com>
2018-12-05 13:32:56 +0800

    fix bugs

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,21 @@
 <template>
   <div id="app">
+    <!-- todo: add nav bar -->
     <BContainer>
-      <!-- todo: add nav bar -->
       <RouterView />
     </BContainer>
-    <footer class="b-footer">
-      Code by <a
-        target="_blank"
-        href="https://github.com/himself65/"
-      >
-        Himself65
-      </a> |
-      <a
-        target="_blank"
-        href="https://github.com/Himself65/Himself65.github.io/blob/master/LICENSE"
-      >
-        MIT
-      </a>
-      LICENSE |
-      <a
-        target="_blank"
-        href="https://github.com/Himself65/kuen"
-      >
-        KUEN
-      </a>
-      Framework
-    </footer>
+    <Footer />
   </div>
 </template>
+<script>
+import Footer from './components/Footer'
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
 
 <style lang="stylus">
   @import "~@kuen/components/src/stylus/app.styl"
@@ -46,28 +34,5 @@
     font-family $font-size-default
     text-align center
     color #2c3e50
-  }
-
-  .b-footer {
-    position relative
-    margin-left auto
-    margin-right auto
-    box-sizing border-box
-    margin-top 1rem
-    margin-bottom 7rem
-    width 80%
-    height 100%
-
-    text-align center
-    font-size 12px
-    color #99a2aa
-
-    a {
-      color: $blue['base']
-      outline none
-      text-decoration none
-      cursor pointer
-      white-space nowrap
-    }
   }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,57 @@
 <template>
-
+  <footer class="b-footer">
+    Code by <a
+      target="_blank"
+      href="https://github.com/himself65/"
+    >
+      Himself65
+    </a> |
+    <a
+      target="_blank"
+      href="https://github.com/Himself65/Himself65.github.io/blob/master/LICENSE"
+    >
+      MIT
+    </a>
+    LICENSE |
+    <a
+      target="_blank"
+      href="https://github.com/Himself65/kuen"
+    >
+      KUEN
+    </a>
+    Framework
+  </footer>
 </template>
 
 <script>
-  export default {
-    name: "Footer"
-  }
+export default {
+  name: 'Footer'
+}
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+  @import "~@kuen/components/src/stylus/setting.styl"
 
+  .b-footer {
+    position relative
+    margin-left auto
+    margin-right auto
+    box-sizing border-box
+    margin-top 1rem
+    margin-bottom 7rem
+    width 80%
+    height 100%
+
+    text-align center
+    font-size 12px
+    color #99a2aa
+
+    a {
+      color: $blue['base']
+      outline none
+      text-decoration none
+      cursor pointer
+      white-space nowrap
+    }
+  }
 </style>
