b7c94c97ed2cdfff809f9be340c656f070e7e553
himself65 <himself6565@gmail.com>
2018-12-10 01:14:02 +0800

    SUPPORT mock data

---
+++ mock/checkin.js
@@ -0,0 +1,15 @@
+module.exports = (req, res) => {
+  if (req.query) {
+    res.json({
+      '1': { 'rating': 51, 'times_all': 2, 'times_month': 2, 'date': '2018-12-08' },
+      '2': { 'rating': 39, 'times_all': 2, 'times_month': 2, 'date': '2018-12-08' },
+      '3': { 'rating': 24, 'times_all': 1, 'times_month': 1, 'date': '2018-12-08' },
+      '4': { 'rating': 10, 'times_all': 1, 'times_month': 1, 'date': '2018-12-08' },
+      '5': { 'rating': 19, 'times_all': 1, 'times_month': 1, 'date': '2018-12-09' },
+      '6': { 'rating': 18, 'times_all': 1, 'times_month': 1, 'date': '2018-12-08' },
+      '7': { 'rating': 107, 'times_all': 5, 'times_month': 5, 'date': '2018-12-10' }
+    })
+  } else {
+    res.json(['1', '2', '3'])
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,14 @@ module.exports = {
   pwa: {
     name: 'Himself65',
     themeColor: '#6cb2eb'
+  },
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: false
+      }
+    }
   }
 }
