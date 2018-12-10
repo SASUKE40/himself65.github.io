434dfd639484cab2a28e046c07ec593b06692708
himself65 <himself6565@gmail.com>
2018-12-10 12:56:11 +0800

    UPDATE

--- src/views/Checkin.vue
+++ src/views/Checkin.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="width-wrap">
+    <!-- fixme error on b-width-wrap -->
     <div slot="main" class="fix-main">
       <BCard>
         <BCardHeader title="积分榜" />
         <BCardBody>
-          <div>
-            {{ membersInfo }}
-          </div>
+          <table class="table">
+            <tr>
+              <th>QQ ID</th>
+              <th>积分</th>
+              <th>总签到次数</th>
+              <th>该月签到次数</th>
+              <th>上次签到</th>
+            </tr>
+            <tr v-for="(member, idx) in membersInfo" :key="member">
+              <td>{{ idx }}</td>
+              <td>{{ member.rating }}</td>
+              <td>{{ member.times_all }}</td>
+              <td>{{ member.times_month }}</td>
+              <td>{{ member.date }}</td>
+            </tr>
+          </table>
         </BCardBody>
       </BCard>
     </div>
@@ -19,7 +33,7 @@
               v-for="group in groups"
               :key="group"
               class="group"
-              @click="membersInfo = getUsersInfo(group)"
+              @click="getUsersInfo(group)"
             >
               {{ group }}
             </div>
@@ -44,18 +58,18 @@ export default {
   },
 
   async mounted () {
-    this.groups = await this.getAllGroups()
+    await this.getAllGroups()
   },
 
   methods: {
     async getAllGroups () {
-      return axios.get('/api/checkin').then(res => {
+      this.groups = await axios.get('/api/checkin').then(res => {
         return res.data
       })
     },
 
     async getUsersInfo (val) {
-      return axios.get('/api/checkin', { params: { id: val } }).then(res => {
+      this.membersInfo = await axios.get('/api/checkin', { params: { id: val } }).then(res => {
         return res.data
       })
     }
@@ -83,6 +97,16 @@ export default {
     }
   }
 
+  .table {
+    margin-left: auto
+    margin-right: auto
+    text-align: center
+
+    th {
+      padding: .3rem .5rem
+    }
+  }
+
   .group {
     padding-top: 1rem
     color: #6CB2EB
@@ -94,14 +118,14 @@ export default {
   }
 
   .fix-main {
-    flex: 6 1 auto
+    flex: 7 1 auto
     @media $display-breakpoints.sm-and-down {
       margin-top: 1.5rem
     }
   }
 
   .fix-side {
-    flex: 4 0 auto
+    flex: 3 0 auto
     margin-left: 1.5rem
     @media $display-breakpoints.sm-and-down {
       margin-top 1rem
