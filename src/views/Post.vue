2a4c7b8c8c34c0b14197fb56f9c1570ac6138347
himself65 <himself6565@gmail.com>
2019-02-01 17:41:39 +0800

    swap postLoader to `astoria`

---
+++ src/views/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    content: { type: String, required: true }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
