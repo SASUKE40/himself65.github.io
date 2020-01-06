12109b7232417fe7a7d6a415b325503aa952ab65
himself65 <himself65@outlook.com>
2020-01-06 12:30:49 +0800

    fix: code highlighting

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import "prismjs/themes/prism-solarizedlight.css"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
