186acaa265cd5cafc7a92daa0f3bb5307b15f707
himself65 <himself65@outlook.com>
2020-01-06 15:24:54 +0800

    style: lint fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Himself65 Blog`,
-    author: `Himself65`,
-    description: `Studying, Gaming, and Coding`,
-    siteUrl: `https://himself65.com/`,
+    title: 'Himself65 Blog',
+    author: 'Himself65',
+    description: 'Studying, Gaming, and Coding',
+    siteUrl: 'https://himself65.com/',
     social: {
-      twitter: `himself_65`,
-      github: `himself65`
+      twitter: 'himself_65',
+      github: 'himself65'
     }
   },
   plugins: [
     {
-      resolve: `gatsby-remark-prismjs`,
+      resolve: 'gatsby-remark-prismjs',
       options: {
-        classPrefix: "language-",
+        classPrefix: 'language-',
         inlineCodeMarker: null,
         showLineNumbers: false,
         noInlineHighlight: false,
         languageExtensions: [
           {
-            language: "superscript",
-            extend: "javascript",
+            language: 'superscript',
+            extend: 'javascript',
             definition: {
               superscript_types: /(SuperType)/
             },
@@ -34,71 +34,71 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`
+        name: 'blog'
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`
+        name: 'assets'
       }
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590
             }
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
+              wrapperStyle: 'margin-bottom: 1.0725rem'
             }
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants'
         ]
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-110549153-1"
+        trackingId: 'UA-110549153-1'
       }
     },
-    `gatsby-plugin-feed`,
+    'gatsby-plugin-feed',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Himself65 Blog`,
-        short_name: `Himself65`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/himself65.jpg`
+        name: 'Himself65 Blog',
+        short_name: 'Himself65',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'content/assets/himself65.jpg'
       }
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/typography`
+        pathToConfigModule: 'src/utils/typography'
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ]
-};
+}
