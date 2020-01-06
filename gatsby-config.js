57cefe246aa87be7e543b94cd65aec8fd5c195cd
himself65 <himself65@outlook.com>
2020-01-06 11:54:47 +0800

    feat: use Gatsby instead of Vuepress

---
+++ gatsby-config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteMetadata: {
+    title: `Himself65 Blog`,
+    author: `Himself65`,
+    description: `Studying, Gaming, and Coding`,
+    siteUrl: `https://himself65.com/`,
+    social: {
+      twitter: `himself_65`,
+      github: `himself65`
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-110549153-1'
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Himself65 Blog`,
+        short_name: `Himself65`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/himself65.jpg`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/bio.js
@@ -0,0 +1,75 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+      query BioQuery {
+          avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+              childImageSharp {
+                  fixed(width: 50, height: 50) {
+                      ...GatsbyImageSharpFixed
+                  }
+              }
+          }
+          site {
+              siteMetadata {
+                  author
+                  social {
+                      twitter
+                  }
+              }
+          }
+      }
+  `)
+
+  const { author, social } = data.site.siteMetadata
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+      }}
+    >
+      <Image
+        fixed={data.avatar.childImageSharp.fixed}
+        alt={author}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          minWidth: 50,
+          borderRadius: `100%`,
+        }}
+        imgStyle={{
+          borderRadius: `50%`,
+        }}
+      />
+      <p>
+        Written by <strong>{author}</strong>, who contributing to open source,
+        and working from school.
+        {` `}
+        <br/>
+        <span>You can follow him on </span>
+        <a href={`https://twitter.com/${social.twitter}`}>
+          Twitter
+        </a>
+        <br/>
+        <span> Or </span>
+        <a href={`https://himself65.com${social.twitter}`}>
+          Github Account
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default Bio
