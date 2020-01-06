48a9ef86b99df84814c15e22f73fe37e8b0a8de2
himself65 <himself65@outlook.com>
2020-01-06 20:17:06 +0800

    fix: header

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,50 +22,25 @@ class Layout extends React.Component {
   render () {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
+    const header = (
+      <h3
+        style={{
+          fontFamily: 'Montserrat, sans-serif',
+          marginTop: 0
+        }}
+      >
+        <Link
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit'
           }}
+          to={'/'}
         >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit'
-            }}
-            to={'/'}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit'
-            }}
-            to={'/'}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+          {title}
+        </Link>
+      </h3>
+    )
     return (
       <div
         style={{
@@ -124,7 +99,7 @@ class Layout extends React.Component {
               }
             />
           ) : (
-            <div style={{ height: '24px' }} />
+            <div style={{ height: '24px' }}/>
           )}
         </header>
         <main>{children}</main>
