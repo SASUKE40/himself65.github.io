70127d21d163acd1630fa2dc5820562e3632ea78
SASUKE40 <sasuke688848@gmail.com>
2020-01-06 17:34:42 +0800

    feat: add dark mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Toggle from './Toggle'
+import sun from '../assets/sun.png'
+import moon from '../assets/moon.png'
+import Helmet from 'react-helmet'
 
 import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
-  render () {
+  state = {
+    theme: null,
+  }
+  componentDidMount() {
+    this.setState({ theme: window.__theme })
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme })
+    }
+  }
+  render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
     let header
@@ -15,14 +28,14 @@ class Layout extends React.Component {
           style={{
             ...scale(1.5),
             marginBottom: rhythm(1.5),
-            marginTop: 0
+            marginTop: 0,
           }}
         >
           <Link
             style={{
               boxShadow: 'none',
               textDecoration: 'none',
-              color: 'inherit'
+              color: 'inherit',
             }}
             to={'/'}
           >
@@ -35,14 +48,14 @@ class Layout extends React.Component {
         <h3
           style={{
             fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0
+            marginTop: 0,
           }}
         >
           <Link
             style={{
               boxShadow: 'none',
               textDecoration: 'none',
-              color: 'inherit'
+              color: 'inherit',
             }}
             to={'/'}
           >
@@ -54,18 +67,68 @@ class Layout extends React.Component {
     return (
       <div
         style={{
+          color: 'var(--textNormal)',
+          background: 'var(--bg)',
+          // transition: 'color 0.2s ease-out, background 0.2s ease-out',
+          // minHeight: '100vh',
           marginLeft: 'auto',
           marginRight: 'auto',
           maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        <header>{header}</header>
+        <Helmet
+          meta={[
+            {
+              name: 'theme-color',
+              content: this.state.theme === 'light' ? '#ffa8c5' : '#282c35',
+            },
+          ]}
+        />
+        <header
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: '2.625rem',
+          }}
+        >
+          {header}
+          {this.state.theme !== null ? (
+            <Toggle
+              icons={{
+                checked: (
+                  <img
+                    src={moon}
+                    width="16"
+                    height="16"
+                    role="presentation"
+                    style={{ pointerEvents: 'none' }}
+                  />
+                ),
+                unchecked: (
+                  <img
+                    src={sun}
+                    width="16"
+                    height="16"
+                    role="presentation"
+                    style={{ pointerEvents: 'none' }}
+                  />
+                ),
+              }}
+              checked={this.state.theme === 'dark'}
+              onChange={e =>
+                window.__setPreferredTheme(e.target.checked ? 'dark' : 'light')
+              }
+            />
+          ) : (
+            <div style={{ height: '24px' }} />
+          )}
+        </header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {' '}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+          © {new Date().getFullYear()}, Built with{' '}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     )
