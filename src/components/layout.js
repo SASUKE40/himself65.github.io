0ced3425fdab8e9c180fd26bdda4eb97e0de3ddd
himself65 <himself65@outlook.com>
2020-01-07 13:45:12 +0800

    feat: display build time

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,115 +1,117 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useEffect, useState } from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Toggle from './Toggle'
 import sun from '../assets/sun.png'
 import moon from '../assets/moon.png'
 import Helmet from 'react-helmet'
 
-import { rhythm, scale } from '../utils/typography'
+import { rhythm } from '../utils/typography'
+import moment from 'moment'
 
-class Layout extends React.Component {
-  state = {
-    theme: null
-  }
+const Layout = (props) => {
+  const [theme, setTheme] = useState(null)
+  useEffect(() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
 
-  componentDidMount () {
-    this.setState({ theme: window.__theme })
-    window.__onThemeChange = () => {
-      this.setState({ theme: window.__theme })
+  const { title, children } = props
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      site {
+        buildTime
+      }
     }
-  }
+  `)
 
-  render () {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    const header = (
-      <h3
+  const header = (
+    <h3
+      style={{
+        fontFamily: 'Montserrat, sans-serif',
+        marginTop: 0
+      }}
+    >
+      <Link
         style={{
-          fontFamily: 'Montserrat, sans-serif',
-          marginTop: 0
+          boxShadow: 'none',
+          textDecoration: 'none',
+          color: 'inherit'
+        }}
+        to={'/'}
+      >
+        {title}
+      </Link>
+    </h3>
+  )
+  return (
+    <div
+      style={{
+        color: 'var(--textNormal)',
+        background: 'var(--bg)',
+        // transition: 'color 0.2s ease-out, background 0.2s ease-out',
+        // minHeight: '100vh',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+      }}
+    >
+      <Helmet
+        meta={[
+          {
+            name: 'theme-color',
+            content: theme === 'light' ? '#ffa8c5' : '#282c35'
+          }
+        ]}
+      />
+      <header
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: '2.625rem'
         }}
       >
-        <Link
-          style={{
-            boxShadow: 'none',
-            textDecoration: 'none',
-            color: 'inherit'
-          }}
-          to={'/'}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-    return (
-      <div
-        style={{
-          color: 'var(--textNormal)',
-          background: 'var(--bg)',
-          // transition: 'color 0.2s ease-out, background 0.2s ease-out',
-          // minHeight: '100vh',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
-        }}
-      >
-        <Helmet
-          meta={[
-            {
-              name: 'theme-color',
-              content: this.state.theme === 'light' ? '#ffa8c5' : '#282c35'
+        {header}
+        {theme != null ? (
+          <Toggle
+            icons={{
+              checked: (
+                <img
+                  src={moon}
+                  width='16'
+                  height='16'
+                  role='presentation'
+                  style={{ pointerEvents: 'none' }}
+                />
+              ),
+              unchecked: (
+                <img
+                  src={sun}
+                  width='16'
+                  height='16'
+                  role='presentation'
+                  style={{ pointerEvents: 'none' }}
+                />
+              )
+            }}
+            checked={theme === 'dark'}
+            onChange={e =>
+              window.__setPreferredTheme(e.target.checked ? 'dark' : 'light')
             }
-          ]}
-        />
-        <header
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginBottom: '2.625rem'
-          }}
-        >
-          {header}
-          {this.state.theme !== null ? (
-            <Toggle
-              icons={{
-                checked: (
-                  <img
-                    src={moon}
-                    width='16'
-                    height='16'
-                    role='presentation'
-                    style={{ pointerEvents: 'none' }}
-                  />
-                ),
-                unchecked: (
-                  <img
-                    src={sun}
-                    width='16'
-                    height='16'
-                    role='presentation'
-                    style={{ pointerEvents: 'none' }}
-                  />
-                )
-              }}
-              checked={this.state.theme === 'dark'}
-              onChange={e =>
-                window.__setPreferredTheme(e.target.checked ? 'dark' : 'light')
-              }
-            />
-          ) : (
-            <div style={{ height: '24px' }}/>
-          )}
-        </header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with{' '}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
-        </footer>
-      </div>
-    )
-  }
+          />
+        ) : (
+          <div style={{ height: '24px' }}/>
+        )}
+      </header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built {' '}
+        on {moment(data.site.buildTime).format('YYYY D Mo, H:m')}{' '}
+        with <a href='https://www.gatsbyjs.org'>Gatsby</a>
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
