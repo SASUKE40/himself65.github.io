7960804f4156ac82cdec835904ebf2a090f99019
himself65 <himself65@outlook.com>
2020-01-07 17:48:20 +0800

    fix: remove prop-types

--- src/html.js
+++ src/html.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 export default class HTML extends React.Component {
   render () {
     return (
       <html {...this.props.htmlAttributes}>
         <head>
-          <meta charSet='utf-8' />
-          <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+          <meta charSet='utf-8'/>
+          <meta httpEquiv='x-ua-compatible' content='ie=edge'/>
           <meta
             name='viewport'
             content='width=device-width, initial-scale=1, shrink-to-fit=no'
@@ -60,13 +59,4 @@ export default class HTML extends React.Component {
       </html>
     )
   }
-}
-
-HTML.propTypes = {
-  htmlAttributes: PropTypes.object,
-  headComponents: PropTypes.array,
-  bodyAttributes: PropTypes.object,
-  preBodyComponents: PropTypes.array,
-  body: PropTypes.string,
-  postBodyComponents: PropTypes.array
 }
