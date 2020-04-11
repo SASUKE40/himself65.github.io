66e73c111e376f292974cc9f45880ea0c88ca502
himself65 <himself65@outlook.com>
2020-04-11 23:16:08 +0800

    feat: init repl module

---
+++ lib/repl/src/lib.rs
@@ -0,0 +1,16 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn repl(line: &str) -> String {
+    return format!("");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn repl_base() {
+
+    }
+}
